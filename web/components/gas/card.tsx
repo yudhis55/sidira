7b3539e33adce3a3105df6e110abb14e02c8284e
yudhis55 <yudisputra55@gmail.com>
2026-07-20 20:49:41 +0700

    fix(gas): resolve lint errors after modal and purge work
    Fix empty CardProps type and related lint blockers for Final Wave.

--- web/components/gas/card.tsx
+++ web/components/gas/card.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+type CardProps = React.HTMLAttributes<HTMLDivElement>;
 
 const Card = React.forwardRef<HTMLDivElement, CardProps>(
   ({ className, ...props }, ref) => {
